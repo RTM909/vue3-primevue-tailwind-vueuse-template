7bf1cd58417f05b46696831ddfca4bce4b06d51b
Rahul Mehta <rmehta2022@my.fit.edu>
2024-07-04 11:05:51 -0400

    added tailwind and dark mode toggle

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,9 +2,14 @@
 defineProps({
   msg: {
     type: String,
-    required: true
-  }
-})
+    required: true,
+  },
+});
+
+const toggleColorScheme = () => {
+  const element = document.querySelector("html");
+  element.classList.toggle("dark");
+};
 </script>
 
 <template>
@@ -18,6 +23,33 @@ defineProps({
 
     <Button label="Check" icon="pi pi-check"></Button>
     <Button icon="pi pi-home" aria-label="Save" />
+
+    <Button label="Toggle Color Scheme" @click="toggleColorScheme()" />
+
+    <Card style="width: 25rem; overflow: hidden">
+      <template #header>
+        <img
+          alt="user header"
+          src="https://primefaces.org/cdn/primevue/images/usercard.png"
+        />
+      </template>
+      <template #title>Advanced Card</template>
+      <template #subtitle>Card subtitle</template>
+      <template #content>
+        <p class="m-0">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore
+          sed consequuntur error repudiandae numquam deserunt quisquam repellat
+          libero asperiores earum nam nobis, culpa ratione quam perferendis
+          esse, cupiditate neque quas!
+        </p>
+      </template>
+      <template #footer>
+        <div class="mt-1 flex gap-4">
+          <Button label="Cancel" severity="secondary" outlined class="w-full" />
+          <Button label="Save" class="w-full" />
+        </div>
+      </template>
+    </Card>
   </div>
 </template>
 
